3d0dd5338ba21db01c1fe5e9155b2d4e567db17a
User1928374 <andrewochockey@gmail.com>
2025-03-05 13:49:50 -0500

    updated restaurant menu testing

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -5,7 +5,7 @@ const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
 
- let mainCourseItem = '';
+let mainCourseItem = '';
 mainCourseMenu.forEach((item, index) => {
 mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
 document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
@@ -14,3 +14,7 @@ let dessertItem = '';
 for (let i = 0; i < dessertMenu.length; i++) {
     dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
 document.getElementById('dessertMenuItems').innerHTML = dessertItem;
+
+const breakfastMenuPrices = ['Pancakes- $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15'];
+const breakfeastMenuItemsMap = breakfastMenuPrices.map((item,index) => `<p>Item ${index + 1} Price: ${item} <p/>`).join('');
+document.getElementById('breakfeastMenuItemsPrices').innerHTML = breakfeastMenuItemsMap;
