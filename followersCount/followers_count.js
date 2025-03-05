9ac11c0c22bc78d29b5ba3ce50fc0fd8236f62fb
User1928374 <andrewochockey@gmail.com>
2025-03-04 22:20:46 -0500

    added resetCount function and button

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -17,3 +17,9 @@ function checkCountValue() {
 function displayCount() {
     document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
 }
+
+function resetCount(){
+    count = 0;
+    displayCount();
+    alert("Your Followers have been reduced to ashes");
+}
